ce2114e40c9abcf034b42b6a8d32ba83feaeaa45
Antoine Sabot-Durand <antoine@sabot-durand.net>
2024-03-17 18:29:08 +0100

    Correct License text indentation

--- core/src/main/java/org/moditect/model/Version.java
+++ core/src/main/java/org/moditect/model/Version.java
@@ -1,9 +1,9 @@
 /*
  *  SPDX-License-Identifier: Apache-2.0
  *
- *   Copyright The original authors
+ *  Copyright The original authors
  *
- *   Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 package org.moditect.model;
 
