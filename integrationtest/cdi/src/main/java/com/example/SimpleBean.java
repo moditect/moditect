ec74d8c78e09dae74aaffb40d25051754e18b907
Antoine Sabot-Durand <antoine@sabot-durand.net>
2024-03-25 21:35:11 +0100

    Correct licenses

--- integrationtest/cdi/src/main/java/com/example/SimpleBean.java
+++ integrationtest/cdi/src/main/java/com/example/SimpleBean.java
@@ -1,3 +1,10 @@
+/*
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Copyright The original authors
+ *
+ *  Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 package com.example;
 
 import jakarta.enterprise.context.ApplicationScoped;
